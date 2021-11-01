846d002d41c0b40d5a64f4dcf35188ea4dbe61ba
alexmeshr <48737527+alexmeshr@users.noreply.github.com>
2021-11-01 23:40:16 +0300

    test debug

--- Работа/data_sort.py
+++ Работа/data_sort.py
@@ -30,8 +30,8 @@ def sort_data(model, dataloader, device, args):
     model.eval()
     CE = nn.CrossEntropyLoss(reduction='none')
     losses = torch.zeros(len(dataloader.dataset.data))
-    #p_i = torch.zeros(args.num_classes, len(dataloader.dataset.data))
-    #index_i = torch.zeros(args.num_classes)
+    p_i = torch.zeros(args.num_classes, len(dataloader.dataset.data))
+    index_i = torch.zeros(args.num_classes)
     p_max = torch.zeros(len(dataloader.dataset.data))
     answers = torch.zeros(len(dataloader.dataset.data))
     #p_array = np.array([])
@@ -49,15 +49,15 @@ def sort_data(model, dataloader, device, args):
             predictions, nums = torch.max(outputs_p, 1)
             for b in range(inputs.size(0)):
                 losses[index] = loss[b]
-                #p_i[int(nums[b])][int(index_i[int(nums[b])])] = predictions[b]
-                #index_i[int(nums[b])] += 1
+                p_i[int(nums[b])][int(index_i[int(nums[b])])] = predictions[b]
+                index_i[int(nums[b])] += 1
                 p_max[index] = predictions[b]
                 answers[index] = nums[b]
                 index += 1
-    #p_i = [i[i!=0] for i in p_i]
+    p_i = [i[i!=0] for i in p_i]
     losses = (losses - losses.min()) / (losses.max() - losses.min())
     input_loss = losses.reshape(-1, 1)
-    #p_i = [i.reshape(-1,1) for i in p_i]
+    p_i = [i.reshape(-1,1) for i in p_i]
     gmm1 = GaussianMixture(n_components=2, max_iter=10, tol=1e-2, reg_covar=5e-4)
     gmm1.fit(input_loss)
     prob1 = gmm1.predict_proba(input_loss)
@@ -65,22 +65,19 @@ def sort_data(model, dataloader, device, args):
     p_clean = (prob1 > args.p_clean)
     p_noise = (prob1 <= args.p_clean)
     #print("p_clean: ", prob1)
-    #gmm2 = [GaussianMixture(n_components=2, max_iter=10, tol=1e-2, reg_covar=5e-4) for x in range(args.num_classes)]
-    #for i in range(args.num_classes):
-    #    gmm2[i].fit(p_i[i])
-    #prob2 = torch.zeros(len(dataloader.dataset.data))
-    #for j in range(len(dataloader.dataset.data)):
-    #    prob2[j] = gmm2[int(answers[j])].predict_proba([[p_max[j]]])[0][gmm2[int(answers[j])].means_.argmin()]
-    #prob2 = gmm2.predict_proba(p_max)
-    #prob2 = prob2[:, gmm2.means_.argmin()]
-    #p_right = (prob2 > args.p_right)
+    gmm2 = [GaussianMixture(n_components=2, max_iter=10, tol=1e-2, reg_covar=5e-4) for x in range(args.num_classes)]
+    for i in range(args.num_classes):
+        gmm2[i].fit(p_i[i])
+    prob2 = torch.zeros(len(dataloader.dataset.data))
+    for j in range(len(dataloader.dataset.data)):
+        prob2[j] = gmm2[int(answers[j])].predict_proba([[p_max[j]]])[0][gmm2[int(answers[j])].means_.argmin()]
+    prob2 = gmm2.predict_proba(p_max)
+    prob2 = prob2[:, gmm2.means_.argmin()]
+    p_right = (prob2 > args.p_right)
     #p_wrong = (prob2 <= args.p_right)
     #print("p_right: ", prob2)
-    fig, (ax2, ax) = plt.subplots(2,1, figsize=(10, 8))
+    fig, ax2 = plt.subplots(1,1, figsize=(10, 8))
     x = [x for x in range(1000)]
-    #ax.scatter(x = x, y=p_max[p_right][:1000], c = 'g', label='P_right')
-    #ax.scatter(x = x, y=p_max[p_wrong][:1000], c = 'r', label='not P_right')
-    #plt.legend(loc='upper right')
     ax2.scatter(x = x, y=p_max[p_clean][:1000], c = 'b', label='P_clean')
     ax2.scatter(x = x, y=p_max[p_noise][:1000], c = 'y', label='not P_clean')
     plt.legend(loc='upper right')
@@ -109,7 +106,6 @@ def sort_data(model, dataloader, device, args):
                     FN+=1
                 else:
                     TN+=1
-                    print("err")
         precision = TP/(TP + FP)
         recall = TP/(TP + FN)
         F1 = 2 * (recall * precision) / (recall + precision)
