babd1e7c06e76608c5d8a6c72031010c28f49f23
alexmeshr <48737527+alexmeshr@users.noreply.github.com>
2021-11-02 12:17:40 +0300

    test debug

--- Работа/data_sort.py
+++ Работа/data_sort.py
@@ -67,7 +67,9 @@ def sort_data(model, dataloader, device, args):
     #print("p_clean: ", prob1)
     gmm2 = [GaussianMixture(n_components=2, max_iter=10, tol=1e-2, reg_covar=5e-4) for x in range(args.num_classes)]
     for i in range(args.num_classes):
-        gmm2[i].fit(p_i[i])
+        print(p_i)
+        if len(p_i) > 0:
+            gmm2[i].fit(p_i[i])
     prob2 = torch.zeros(len(dataloader.dataset.data))
     for j in range(len(dataloader.dataset.data)):
         prob2[j] = gmm2[int(answers[j])].predict_proba([[p_max[j]]])[0][gmm2[int(answers[j])].means_.argmin()]
