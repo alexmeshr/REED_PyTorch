66159841667129f965822296b40d15e7e6fffa11
alexmeshr <mao2501@rambler.ru>
2022-04-06 01:45:54 +0300

    add resnet_classic.py

--- Работа/models/resnet_simclr.py
+++ Работа/models/resnet_simclr.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 import torchvision.models as models
 from models.resnet import resnet50, resnet18
+from models.resnet_cifar import ResNet50, ResNet18
 from exceptions.exceptions import InvalidBackboneError
 
 
@@ -10,7 +11,10 @@ class ResNetSimCLR(nn.Module):
     def __init__(self, model_name, out_dim):
         super(ResNetSimCLR, self).__init__()
         self.resnet_dict = {"resnet18": resnet18(pretrained=False),
-                            "resnet50": resnet50(pretrained=False)}
+                            "resnet50": resnet50(pretrained=False),
+                            "resnet18_cifar": ResNet18(),
+                            "resnet50_cifar": ResNet50(),
+                           }
         self.num_classes = out_dim
         self.backbone = self.resnet_dict[model_name]
         self.backbone.eval
